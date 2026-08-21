709abcd2758253432918e4489f5394f2d3f37c9f
Daniel Palomino <daniel.palomino-s@escuelaing.edu.co>
2026-08-21 09:35:14 -0500

    Editting hola.cpp from dev branch

--- ejemplo_hw/hola.cpp
+++ ejemplo_hw/hola.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(int argc, char** argv ){
-	printf("Hello World.\n" );
+	printf("Hello World from dev branch.\n" );
 	printf("Another line, for git learning.\n" );
-	printf("Trird line.\n" );
+	printf("Eddditing example from dev branch.\n" );
 	return 0;
 }
